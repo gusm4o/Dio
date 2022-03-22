4a382b6d0da0121c7d2225a4779e22b11cdc7c21
gusmao <anderglara@gmail.com>
2022-03-22 15:07:44 -0400

    Inicio do Git no Pycharm

---
+++ Cognizant_2/Calculo_triangulo.py
@@ -0,0 +1,13 @@
+lados = [float(x) for x in input().split()]
+
+a = lados[0];
+b = lados[1];
+c = lados[2];
+
+if a + b > c and a + c > b and b + c > a:
+    #TODO Preencha a formula do perímeto do triangulo (soma de todos os lados).
+    for i in lado
+    print(f"Perimetro = {    :.1f}")
+else:
+    #TODO Preencha a formula da área do trapézio: AREA = ((A + B) x C) / 2
+    print(f"Area = {    :.1f}")
---
+++ Cognizant_2/media.py
@@ -0,0 +1,8 @@
+a = float(input())
+b = float(input())
+
+#TODO: Complete os espaços em branco com as respectivas variáveis para o cálculo da média.
+media = (a * 3.5 +  b * 7.5) / 11
+
+#TODO: Complete com a variável que representa o resultado da média.
+print(f'MEDIA = { media: .5f}')
